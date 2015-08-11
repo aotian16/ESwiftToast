d62bd594c351f9c7ab0e01c9a62d8972e7594641
tongjin <tongjin@51cailai.com>
2015-08-11 10:02:06 +0800

    new

---
+++ ESwiftToastDemo/ESwiftToastDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ESwiftToastDemo
+//
+//  Created by 童进 on 15/8/11.
+//  Copyright (c) 2015年 qefee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
