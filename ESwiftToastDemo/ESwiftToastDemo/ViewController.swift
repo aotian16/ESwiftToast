fac4e968881c2a069843d3c3b7cf8b97b3f943b2
tongjin <tongjin@51cailai.com>
2015-08-11 10:27:22 +0800

    add ESwiftToast

--- ESwiftToastDemo/ESwiftToastDemo/ViewController.swift
+++ ESwiftToastDemo/ESwiftToastDemo/ViewController.swift
@@ -20,6 +20,38 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onTopButtonClick(sender: UIButton) {
+        ESwiftToast.show("top", duration: ESwiftToast.DEFAULT_DISPLAY_DURATION, position: .top)
+    }
+    
+    @IBAction func onCenterButtonClick(sender: UIButton) {
+        ESwiftToast.show("center", duration: ESwiftToast.DEFAULT_DISPLAY_DURATION, position: .center)
+    }
+    
+    @IBAction func onBottomButtonClick(sender: UIButton) {
+        ESwiftToast.show("bottom")
+    }
+    
+    @IBAction func onActionButtonClick(sender: UIButton) {
+        ESwiftToast.show("click to fire action", tapAction: { (toast) -> Void in
+            ESwiftToast.show("toast taped", duration: ESwiftToast.DEFAULT_DISPLAY_DURATION, position: .center)
+        })
+    }
+    
+    @IBAction func onCustomButtonClick(sender: UIButton) {
+        let toast = ESwiftToast.make("click to fire custom action")
+        
+        toast._offset = 30
+        toast._duration = 3
+        toast._position = .bottom
+        toast._tapAction = {(toast) -> Void in
+            ESwiftToast.show("custom toast taped", duration: ESwiftToast.DEFAULT_DISPLAY_DURATION, position: .center)}
+        toast._animationDuration = 1
+        toast._textLabel.textColor = UIColor.redColor()
+        toast._contentView.backgroundColor = UIColor.yellowColor()
+        
+        toast.innerShow()
+    }
 
 }
 
